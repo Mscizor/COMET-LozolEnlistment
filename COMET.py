a9f16516c17a1c18bfeb164da5213ab764e8bf4f
Mscizor <janurielm@gmail.com>
2019-11-30 12:42:35 +0800

    Elaborated on design, added main

--- COMET.py
+++ COMET.py
@@ -26,10 +26,10 @@
         User Privileges ::
             Admin --
                 Create Class
-                    Classroom Number (String)
-                    Name of Class (String)
-                    Number of Units (Integer)
-                    Classes as Prerequisites ([Class])
+                    Classroom Number(String)
+                    Name of Class(String)
+                    Number of Units(Integer)
+                    Classes as Prerequisites([Class])
 
                 Remove Class
             
@@ -38,74 +38,96 @@
                 Drop Class
 """
 
-class User (object):
-    def __init__ (self, username, password):
+class User(object):
+    def __init__(self, username, password):
         self.username = username
         self.password = password
     
-class Admin (User):
-    def __init__ (self, username, password):
-        super ().__init__ (username, password)
+class Admin(User):
+    def __init__(self, username, password):
+        super().__init__(username, password)
     
-    def __str__ (self):
-        return 'Admin: {}'.format (self.username)
+    def __str__(self):
+        return 'Admin: {}'.format(self.username)
         
-class Student (User):
-    def __init__ (self, id_number, password, name, unit_limit):
-        super ().__init__ (id_number, password)
+class Student(User):
+    def __init__(self, id_number, password, name, unit_limit):
+        super().__init__(id_number, password)
         self.name = name
         self.unit_limit = unit_limit
     
-    def __str__ (self):
-        return 'Student: {} ({}) [{}] - {}'.format (self.username, self.id_number, self.name, self.unit_limit)
+    def __str__(self):
+        return 'Student: {}({}) [{}] - {}'.format(self.username, self.id_number, self.name, self.unit_limit)
 
 class Class:
-    def __init__ (self, name, units, prereqs):
+    def __init__(self, name, units, prereqs):
         self.name = name
         self.units = units
         self.prereqs = prereqs
         
-    def set_name (self, new_name):
+    def set_name(self, new_name):
         self.name = new_name
         
-    def set_units (self, new_units):
+    def set_units(self, new_units):
         self.units = new_units
             
-    def add_prereq (self, new_prereq):
-        self.prereqs.append (new_prereq)
+    def add_prereq(self, new_prereq):
+        self.prereqs.append(new_prereq)
     
-    def remove_prereq (self, index):
+    def remove_prereq(self, index):
         del self.prereqs[index]
     
-    def __str__ (self):
-        str = '[ {}: {} - [ '.format (self.name, self.units)
+    def __str__(self):
+        str = '[ {}: {} - [ '.format(self.name, self.units)
         for c in self.prereqs:
-            str += '{} '.format (c.name)
+            str += '{} '.format(c.name)
         str += ']]'
         return str
 
-def design_bar (char, num, end = '\n'):
-    str = ""
+def design_line(s, num):
+    '''Returns a line of the specified string a number of times.
+    
+    Args:
+        s: String to be printed
+        num: The amount of times 's' is printed
+        end: End of the line
+    
+    Returns:
+        A string containing a line of the given string repeated several times.
+        Given '=' and 5, for example:
+
+        '====='
+
+        Returns nothing if num is less than 0.
+    '''
+
+    if num < 0:
+        return ''
+    str = []
     for _ in range(num):
-        str += char
-    print (str, end = end)
+        str.append (s)
+    return ''.join(str)
 
-classes = []
-users = []
-users.append (Admin ('admin', 'admin'))
+def main():
+    classes = []
+    users = []
+    users.append(Admin('admin', 'admin'))
 
-design_bar ('=', 100)
-username = input ('Lozol Account Name: ')
-password = input ('Lozol Account Password: ')
-design_bar ('=', 100)
+    design_line('=', 100)
+    username = input('Lozol Account Name: ')
+    password = input('Lozol Account Password: ')
+    design_line('=', 100)
 
-found = False
-for user in users:
-    if user.username == username and user.password == password:
-        found = True
-        break
+    found = False
+    for user in users:
+        if user.username == username and user.password == password:
+            found = True
+            break
 
-if not found:
-    print ('Invalid login details.')
-else:
-    print ('Login found. Proceeding to log-in page.')
+    if not found:
+        print('Invalid login details.')
+    else:
+        print('Login found. Proceeding to log-in page.')
+
+if __name__ == '__main__':
+    main()
