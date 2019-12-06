5a7a4b4af9b05e96461f028cf9e3a6d8f67f9841
Mscizor <janurielm@gmail.com>
2019-12-06 20:42:23 +0800

    V1.01, can exit functions and repeat actions

--- COMET.py
+++ COMET.py
@@ -39,11 +39,23 @@
 """
 
 class User:
+    '''A user which may be either an Admin or a Student
+
+    Attributes:
+        username: The username used to log in to Lozol
+        password: The password used to log in to Lozol
+    '''
     def __init__(self, username, password):
         self.username = username
         self.password = password
     
 class Admin(User):
+    '''An user which can create and remove classes and courses.
+
+    Attributes:
+        username: The username used to log in to Lozol
+        password: The password used to log in to Lozol
+    '''
     def __init__(self, username, password):
         super().__init__(username, password)
     
@@ -51,6 +63,14 @@ class Admin(User):
         return f'Admin / {self.username} / {self.password}'
         
 class Student(User):
+    '''A user which can enrol and drop classes, as well as have previously enrolled in courses.
+
+    Attributes:
+        username: The username used to log in to Lozol, also the student's ID number
+        password: The password used to log in to Lozol
+        name: The actual name of the student
+        unit_limit: The maximum amount of units the student is allowed to enrol in at a time
+    '''
     def __init__(self, id_number, password, name, unit_limit):
         super().__init__(id_number, password)
         self.name = name
@@ -60,6 +80,13 @@ class Student(User):
         return f'Student / {self.username} / {self.password} / {self.name} / {self.unit_limit}'
 
 class Course:
+    '''Can have many classes teaching this course, as well as be a prerequisite to other courses.
+
+    Attributes:
+        course_name: The actual name of the course
+        units: The amount of units the course is worse
+        prereqs: A list of the names of the prerequisite courses this course requires
+    '''
     def __init__(self, course_name, units, prereqs):
         self.course_name = course_name
         self.units = units
@@ -75,6 +102,13 @@ class Course:
         return ''.join(info)
 
 class Class:
+    '''Teaches a course, can have many students enrolled in a class.
+
+    Attributes:
+        course_name: The name of the course the class is teaching
+        classroom: The classroom name or location
+        student_ids: A list of the student_ids enrolled in this class
+    '''
     def __init__(self, course_name, classroom, student_ids):
         self.course_name = course_name
         self.classroom = classroom
@@ -87,6 +121,12 @@ class Class:
         return ''.join(info)
 
 class PrevEnrolments:
+    '''Links students and courses that each student has previously been enrolled in.
+
+    Attributes:
+        student_id: The student that the previously enrolled courses are referring to
+        prev_enrolled: A list of all the courses that the student has previously been enrolled in
+    '''
     def __init__(self, student_id, prev_enrolled):
         self.student_id = student_id
         self.prev_enrolled = prev_enrolled
@@ -222,16 +262,18 @@ def enrol_class(student, courses, classes, prev_enrolments):
             curr_enrolled = set()
             courses_enrolled = set()
             units_remaining = student.unit_limit
+
+            # Counts how many units student has left
             for cl in classes.values():
                 if student.username in cl.student_ids:
                     courses_enrolled.add(cl.course_name)
                     curr_enrolled.add(f'{cl.course_name} / {cl.classroom}')
                     units_remaining -= courses[cl.course_name].units
 
+            # Gets all available classes that the student can enrol in (units fit, course not currently/previously enrolled)
             avail_classes = set()
             for cl in classes.values():
                 course = courses[cl.course_name]
-                # Lengthy way of checking if can fit in units and the course of the class has not been previously or currently enrolled
                 if course.units <= units_remaining and cl.course_name not in courses_enrolled and cl.course_name not in prev_enrolments:
                     for prereq in course.prereqs:
                         if prereq not in prev_enrolments:
@@ -315,6 +357,7 @@ def drop_class(student, classes):
     
     try:
         while True:
+            # Gets all classes student is currently enrolled in
             curr_enrolled = set()
             for cl in classes.values():
                 if student.username in cl.student_ids:
@@ -423,6 +466,8 @@ def create_class(courses, classes):
                     print('Course name is too long (>= 20 characters), please try again.')
                 elif len(classroom) >= 15:
                     print('Classroom name is too long (>= 15 characters), please try again.')
+                elif ' ' in classroom:
+                    print('Classroom name must have no spaces, please try again.')
                 elif f'{course_name} / {classroom}' in classes:
                     print('Class with same name and classroom already found, please try again.')
                 elif course_name not in courses:
@@ -546,6 +591,8 @@ def create_course(courses):
                     print('Course name cannot be blank, please try again.')
                 elif len(course_name) >= 20:
                     print('Course name is too long (>= 20 characters), please try again.')
+                elif ' ' in course_name:
+                    print('Course name cannot have spaces, please try again.')
                 elif course_name in courses:
                     print('Course with same name already found, please try again.')
                 else:
@@ -561,7 +608,7 @@ def create_course(courses):
                     else:
                         break
                 except ValueError:
-                    print('Input was not an integer, please try again.') 
+                    print('Input was not a single integer, please try again.') 
                 print(design_line('-', 100))
 
             prereqs = None
@@ -614,10 +661,10 @@ def remove_course(courses, classes):
 
     try:
         while True:
+            # Unavailable courses are courses which have a class or is a prerequisite of another course
             unavail_courses = set()
             for course in courses.values():
                 unavail_courses.update(course.prereqs)
-
             for cl in classes.values():
                 unavail_courses.add(cl.course_name)
             
@@ -684,112 +731,153 @@ def main():
     classes = {}
     prev_enrolments = {}
 
-    with open('users.txt') as users_txt:
-        for user in users_txt.readlines():
-            parsed_user = parse_as_user(user.strip())
-            if parsed_user is not None:
-                users[parsed_user.username] = parsed_user
-            
-    with open('courses.txt') as courses_txt:
-        for course in courses_txt.readlines():
-            parsed_course = parse_as_course(course.strip('\n'))
-            if parsed_course is not None:
-                courses[parsed_course.course_name] = parsed_course
+    try:
+        with open('users.txt') as users_txt:
+            for user in users_txt.readlines():
+                parsed_user = parse_as_user(user.strip())
+                if parsed_user is not None:
+                    users[parsed_user.username] = parsed_user
+                
+        with open('courses.txt') as courses_txt:
+            for course in courses_txt.readlines():
+                parsed_course = parse_as_course(course.strip('\n'))
+                if parsed_course is not None:
+                    courses[parsed_course.course_name] = parsed_course
 
-    with open('classes.txt') as classes_txt:
-        for cl in classes_txt.readlines():
-            parsed_class = parse_as_class(cl.strip('\n'))
-            if parsed_class is not None:
-                classes[f'{parsed_class.course_name} / {parsed_class.classroom}'] = parsed_class
+        with open('classes.txt') as classes_txt:
+            for cl in classes_txt.readlines():
+                parsed_class = parse_as_class(cl.strip('\n'))
+                if parsed_class is not None:
+                    classes[f'{parsed_class.course_name} / {parsed_class.classroom}'] = parsed_class
 
-    with open('prev_enrolments.txt') as prev_enrolments_txt:
-        for p_enrol in prev_enrolments_txt.readlines():
-            parsed_p_enrol = parse_as_prev_enrolments(p_enrol.strip('\n'))
-            if parsed_p_enrol is not None:
-                prev_enrolments[parsed_p_enrol.student_id] = parsed_p_enrol
+        with open('prev_enrolments.txt') as prev_enrolments_txt:
+            for p_enrol in prev_enrolments_txt.readlines():
+                parsed_p_enrol = parse_as_prev_enrolments(p_enrol.strip('\n'))
+                if parsed_p_enrol is not None:
+                    prev_enrolments[parsed_p_enrol.student_id] = parsed_p_enrol
 
-    exit_login = False
-    login_user = None
-    while True:
-        print(design_line('=', 100))
-        username = input('Lozol Account Username: ')
-        password = input('Lozol Account Password: ')
-        print(design_line('=', 100))
+        exit_login = False
+        login_user = None
 
-        found = False
-        if username not in users or users[username].password != password:
-            print('Invalid login details.')
-            while True:
-                query = input('Exit program? (y or n): ').lower()
-                if 'y' in query: 
-                    exit_login = True
-                    break
-                elif 'n' in query:
-                    break
-        else:
-            login_user = users[username]
-            print('Login found. Proceeding to user page.')
-            break
-            
-    if not exit_login:
-        if isinstance(login_user, Admin):
-            print(f'Welcome to your administrator dashboard, {login_user.username}')
-            print('[1] Create Class')
-            print('[2] Remove Class')
-            print('[3] Create Course')
-            print('[4] Remove Course')
-            num = int(input('Please enter your choice: '))
+        while True:
             print(design_line('=', 100))
-            if num == 1:
-                create_class(courses, classes)
-            elif num == 2:
-                remove_class(classes)
-            elif num == 3:
-                create_course(courses)
-            elif num == 4:
-                remove_course(courses, classes)
-        elif isinstance(login_user, Student):
-            print(f'Welcome to your Lozol Account, {login_user.name}')
-            print('[1] Enrol in Class')
-            print('[2] Drop a Class')
-            num = int(input('Please input your choice: '))
-            if num == 1:
-                enrol_class(login_user, courses, classes, prev_enrolments)
-            elif num == 2:
-                drop_class(login_user, classes)
+            username = input('Lozol Account Username: ')
+            password = input('Lozol Account Password: ')
+            print(design_line('=', 100))
 
-    print (design_line('=', 100))
-    print ('Exiting...')
+            found = False
+            if username not in users or users[username].password != password:
+                print('Invalid login details.')
+                while True:
+                    query = input('Exit program? (y or n): ').lower()
+                    if 'y' in query: 
+                        exit_login = True
+                        break
+                    elif 'n' in query:
+                        break
+            else:
+                login_user = users[username]
+                print('Login found. Proceeding to user page.')
+                break
+                
+        while not exit_login:
+            if isinstance(login_user, Admin):
+                while True:
+                    print(f'Welcome to your administrator dashboard, {login_user.username}')
+                    print('[1] Create Class')
+                    print('[2] Remove Class')
+                    print('[3] Create Course')
+                    print('[4] Remove Course')
+                    print('[5] Exit')
+                    try:
+                        num = int(input('Please enter your choice: '))
+                        if 1 <= num <= 5:
+                            print(design_line('=', 100))
 
-    while True:
-        save = input('Save changes? (y/n): ').lower()
-        if 'y' in save:
-            with open('users.txt', 'w') as users_txt:
-                for i, user in enumerate(users.values()):
-                    users_txt.write(user.info())
-                    if i != len(users.values()) - 1:
-                        users_txt.write('\n')
+                        if num == 1:
+                            create_class(courses, classes)
+                        elif num == 2:
+                            remove_class(classes)
+                        elif num == 3:
+                            create_course(courses)
+                        elif num == 4:
+                            remove_course(courses, classes)
+                        elif num == 5:
+                            exit_login = True
+                        else:
+                            raise ValueError('Not in choices')
 
-            with open('courses.txt', 'w') as courses_txt:
-                for i, user in enumerate(courses.values()):
-                    courses_txt.write(user.info())
-                    if i != len(courses.values()) - 1:
-                        courses_txt.write('\n')
+                        break
+                    except ValueError:
+                        print('Choice selected was not an integer in the choices, please try again.')
+                    finally:
+                        print(design_line('=', 100))
+            elif isinstance(login_user, Student):
+                while True:
+                    print(f'Welcome to your Lozol Account, {login_user.name}')
+                    print('[1] Enrol in Class')
+                    print('[2] Drop a Class')
+                    print('[3] Exit')
+                    try:
+                        num = int(input('Please input your choice: '))
+                        if 1 <= num <= 3:
+                            print(design_line('=', 100))
 
-            with open('classes.txt', 'w') as classes_txt:
-                for i, cl in enumerate(classes.values()):
-                    classes_txt.write(cl.info())
-                    if i != len(classes.values()) - 1:
-                        classes_txt.write('\n')
+                        if num == 1:
+                            enrol_class(login_user, courses, classes, prev_enrolments)
+                        elif num == 2:
+                            drop_class(login_user, classes)
+                        elif num == 3:
+                            exit_login = True
+                        else:
+                            raise ValueError('Not in choices')
 
-            with open('prev_enrolments.txt', 'w') as prev_enrolments_txt:
-                for i, st in enumerate(prev_enrolments.values()):
-                    prev_enrolments_txt.write(st.info())
-                    if i != len(prev_enrolments.values()) - 1:
-                        prev_enrolments_txt.write('\n')
-            break
-        elif 'n' in save:
-            break
+                        break
+                    except ValueError:
+                        print('Choice selected was not an integer in the choices, please try again.')
+                    finally:
+                        print(design_line('=', 100))
+
+        print ('Exiting...')
+    except KeyboardInterrupt:
+        print(design_line('!', 100))
+        print('Forceful exit of program...')
+        print(design_line('!', 100))
+    
+    try:
+        while True:
+            save = input('Save changes? (y/n): ').lower()
+            if 'y' in save:
+                with open('users.txt', 'w') as users_txt:
+                    for i, user in enumerate(users.values()):
+                        users_txt.write(user.info())
+                        if i != len(users.values()) - 1:
+                            users_txt.write('\n')
+
+                with open('courses.txt', 'w') as courses_txt:
+                    for i, user in enumerate(courses.values()):
+                        courses_txt.write(user.info())
+                        if i != len(courses.values()) - 1:
+                            courses_txt.write('\n')
+
+                with open('classes.txt', 'w') as classes_txt:
+                    for i, cl in enumerate(classes.values()):
+                        classes_txt.write(cl.info())
+                        if i != len(classes.values()) - 1:
+                            classes_txt.write('\n')
+
+                with open('prev_enrolments.txt', 'w') as prev_enrolments_txt:
+                    for i, st in enumerate(prev_enrolments.values()):
+                        prev_enrolments_txt.write(st.info())
+                        if i != len(prev_enrolments.values()) - 1:
+                            prev_enrolments_txt.write('\n')
+                break
+            elif 'n' in save:
+                break
+    except KeyboardInterrupt:
+        print(design_line('!', 100))
+        print('Saving interrupted, no changed were saved.')
+        print(design_line('!', 100))
 
 if __name__ == '__main__':
     main()
